b49eb9a313de98d99a721432a17f1db0aac55033
WCharacter <mitayletsplay@gmail.com>
2021-04-14 23:59:09 +0300

    added fix for a bug when event listener for button removes after window was minimized

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
 url = "";
 
-console.log("added listener");
 chrome.webRequest.onCompleted.addListener(
     function(details) {    
 		if(details.url.includes("GeoPhotoService.GetMetadata") && details.initiator == "https://www.geoguessr.com"){
 			fetch(details.url)
 				.then(response => response.text())
 				.then(text => {
-					coordinates = [...text.matchAll(/null,null,-?\d{1,3}\.\d{11,14},-?\d{1,3}\.\d{11,14}/g)][0][0].substring(10);	
-					url = `http://maps.google.com/?q=${coordinates}`;
-					console.log(url);
-					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-						chrome.tabs.sendMessage(tabs[0].id, {action: "setUrl", url: url});
-					});
+					matches = [...text.matchAll(/null,null,-?\d{1,3}\.\d{11,14},-?\d{1,3}\.\d{11,14}/g)]
+					if(matches.length !== 0){
+						coordinates = matches[0][0].substring(10);	
+						url = `http://maps.google.com/?q=${coordinates}`;
+						console.log(url);
+						chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+							chrome.tabs.sendMessage(tabs[0].id, {action: "setUrl", url: url});
+						});
+					}
 				});								
 		}
 	},    
--- buttonContent.js
+++ buttonContent.js
@@ -1,21 +1,30 @@
 var url = "";
 
+var clickListener = function(){
+	if(url.length !== 0) {
+		window.open(url);	
+	}
+}
+
 var addButton = function(){
 	a = document.getElementsByClassName("header__items")[0];
 	if(a){
 		b = document.createElement("button");
 		b.id = "google-maps-button";
 		b.innerText = "Show on map";
-		b.addEventListener("click", () => {
-			window.open(url);
-		});
+		b.addEventListener("click", clickListener);
 		b.style = "position:relative; top:12px; font-weight: 600;";
 		a.appendChild(b);
 		setInterval(
-			() => chrome.runtime.sendMessage({action: "getUrl"}, function(response) {
-				url = response.url;
-			}), 100
-		);
+			() => 
+			{
+				if(getEventListeners(b).length === 0){
+					b.addEventListener("click", clickListener);
+				}
+				chrome.runtime.sendMessage({action: "getUrl"}, function(response) {
+					url = response.url;
+				})
+			}, 100);
 	}
 }
 
@@ -24,7 +33,9 @@ window.onload = addButton;
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if(request.action === "setUrl"){
-			url = request.url;
+			if(request.url.length !== 0){
+				url = request.url;
+			}
 		}
 	}
 );
